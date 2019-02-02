ddac24c32085169e0683bccc64b27dcd576fefec
Arjun Thippeswamy <ht.arjun@outlook.com>
2019-02-02 13:22:15 -0600

    Correct use of reducer function

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-function todos (action = [], reducer) {
-	if(action.type === 'ADD_TODO') {
-		return state.concat([action.todo]);
-	}
-	
-	return state;
-}
-
-function createStore () {
+// Library Code
+function createStore (reducer) {
 	// The store should have four parts
 	// 1. The state
 	// 2. Get the state.
@@ -31,7 +24,7 @@ function createStore () {
 		It receives the action which is the specific event that occured within the app
 	*/
 	const dispatch = (action) => {
-		state = todos(state, action);
+		state = reducer(state, action);
 		
 		// Loop through all of our listeners (an array of functions) and invoke each one of them
 		listeners.forEach((listener) => listener());
@@ -44,3 +37,19 @@ function createStore () {
 		dispatch
 	}
 }
+
+
+// App code
+function todos (state = [], action) {
+	if(action.type === 'ADD_TODO') {
+		return state.concat([action.todo]);
+	}
+	
+	return state;
+}
+
+/*
+	It doesnn't make sense fr a library have access to todos function because it could be different for different apps and todos might not be in the same scope
+	Therefore we need to pass in the reducer function
+	const store = createStore(todos);
+*/
