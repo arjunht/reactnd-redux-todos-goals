34576c40c19a7b3b9091439dafd0dee7dc7573d2
Arjun Thippeswamy <ht.arjun@outlook.com>
2019-02-02 20:30:43 -0600

    Add constants for action types instead of strings

--- index.js
+++ index.js
@@ -41,13 +41,20 @@ function createStore (reducer) {
 
 
 // App code
+
+const ADD_TODO = 'ADD_TODO';
+const REMOVE_TODO = 'REMOVE_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+const ADD_GOAL = 'ADD_GOAL';
+const REMOVE_GOAL = 'REMOVE_GOAL';
+
 function todos (state = [], action) {
 	switch (action.type) {
-		case 'ADD_TODO' :
+		case ADD_TODO :
 			return state.concat([action.todo]);
-		case 'REMOVE_TODO' :
+		case REMOVE_TODO :
 			return state.filter((todo) => todo.id !== action.id);
-		case 'TOGGLE_TODO' :
+		case TOGGLE_TODO :
 			/* 
 				If the todos reducer function needs to be a pure function, we cannot directly modify the complete property of the todo object in the state
 				Wrong: return state.map((todo) => todo.id === action.id ? todo.complete = !todo.complete : todo)
@@ -65,9 +72,9 @@ function todos (state = [], action) {
 
 function goals (state = [], action) {
 	switch (action.type) {
-		case 'ADD_GOAL' :
+		case ADD_GOAL :
 			return state.concat([action.goal]);
-		case 'REMOVE_GOAL' :
+		case REMOVE_GOAL :
 			return state.filter((goal) => goal.id !== action.id);
 		default :
 			return state;
@@ -109,7 +116,7 @@ store.subscribe(() => {
 });
 
 store.dispatch({
-	type: 'ADD_TODO',
+	type: ADD_TODO,
 	todo: {
 		id: 0,
 		name: 'Learn Redux',
@@ -118,7 +125,7 @@ store.dispatch({
 });
 
 store.dispatch({
-  type: 'ADD_TODO',
+  type: ADD_TODO,
   todo: {
     id: 0,
     name: 'Walk the dog',
@@ -127,7 +134,7 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'ADD_TODO',
+  type: ADD_TODO,
   todo: {
     id: 1,
     name: 'Wash the car',
@@ -136,7 +143,7 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'ADD_TODO',
+  type: ADD_TODO,
   todo: {
     id: 2,
     name: 'Go to the gym',
@@ -145,17 +152,17 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'REMOVE_TODO',
+  type: REMOVE_TODO,
   id: 1
 })
 
 store.dispatch({
-  type: 'TOGGLE_TODO',
+  type: TOGGLE_TODO,
   id: 0
 })
 
 store.dispatch({
-  type: 'ADD_GOAL',
+  type: ADD_GOAL,
   goal: {
     id: 0,
     name: 'Learn Redux'
@@ -163,7 +170,7 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'ADD_GOAL',
+  type: ADD_GOAL,
   goal: {
     id: 1,
     name: 'Lose 20 pounds'
@@ -171,6 +178,6 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'REMOVE_GOAL',
+  type: REMOVE_GOAL,
   id: 0
 })
